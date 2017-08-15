1bc9b93df1aa47ebc35dbfaa52af2f1d5c4b98f2
Andrew James <ajames20@gmail.com>
2017-08-15 16:32:09 +0300

    add image loader gif for images while loading

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,37 +1,49 @@
-$('#campground-search').on('input', function () {
-  var search = $(this).serialize();
+$(window).on('load', () => {
+  // main image loaded ?
+  // $('.thumbnail-image').imagesLoaded({ background: true }, () => {
+  //   $('.loader').remove();
+  //   $('.thumbnail-image').fadeIn('slow');
+  // });
 
-  if (search === "search=") {
-    search = "all";
-  }
-  $.get('/campgrounds?' + search, (data) => {
-    $('#campground-grid').html('');
-    data.forEach((campground) => {
-      $('#campground-grid').append(`
-        <div class="col-xs-12 col-sm-6 col-md-4">
-          <div class="thumbnail">
-          <div class="thumbnail-image" style="background: url('${campground.image}') no-repeat center; background-size: cover; max-height: 250px;"></div>
-            <div class="caption">
-              <h4>${campground.name}</h4>
+  $('.thumbnail-image').imagesLoaded({ background: true }).
+    always((instance) => {
+      console.log('all images loaded');
+      Array.from($('.thumbnail-image')).forEach((div) => {
+        $('.thumbnail-image').fadeIn();
+      });
+    }).done((instance) => {
+      $('.loader').remove();
+    });
+
+
+  // timeout to remove flash message from the DOM
+  setTimeout(() => {
+    $(".message").hide();
+  }, 3000);
+
+  $('#campground-search').on('input', function () {
+    var search = $(this).serialize();
+
+    if (search === "search=") {
+      search = "all";
+    }
+    $.get('/campgrounds?' + search, (data) => {
+      $('#campground-grid').html('');
+      data.forEach((campground) => {
+        $('#campground-grid').append(`
+          <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="thumbnail">
+            <div class="thumbnail-image" style="background: url('${campground.image}') no-repeat center; background-size: cover; max-height: 250px;"></div>
+              <div class="caption">
+                <h4>${campground.name}</h4>
+              </div>
+              <p>
+                <a href="/campgrounds/${campground._id}" class="btn btn-primary">More Info</a>
+              </p>
             </div>
-            <p>
-              <a href="/campgrounds/${campground._id}" class="btn btn-primary">More Info</a>
-            </p>
           </div>
-        </div>
-      `);
+        `);
+      });
     });
   });
-});
-
-// timeout to remove flash message from the DOM
-setTimeout(() => {
-  $(".message").hide();
-}, 3000);
-
-
-// main image loaded ?
-$(window).on('load', () => {
-  // hide/remove the loading image
-  $('.loader').hide();
 });
